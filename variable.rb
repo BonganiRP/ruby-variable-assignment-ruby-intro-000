8cfb1f5229c7c5c28ad6315f59e5f2ce0e3b7204
Richard <phalarichard1@gmail.com>
2020-04-14 15:37:54 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,2 @@
-greeting = "Hello World" # Define a local variable called 'greeting',
+greetings = "Hello World" # Define a local variable called 'greeting',
  puts greeting# give it the value of the string "Hello World"
